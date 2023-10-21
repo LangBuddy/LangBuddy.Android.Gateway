4528bbcbf62ed44c1eedc79ca7505fd770370cdb
fremyxs <antonovch13@mail.ru>
2023-10-21 20:09:35 +0500

    feat: update http service

--- src/service/http/http.service.ts
+++ src/service/http/http.service.ts
@@ -3,23 +3,28 @@ import axios from 'axios';
 
 @Injectable()
 export class HttpService {
-  public async get(url: string): Promise<any> {
-    const response = await axios.get(url);
+  public async get<T>(url: string): Promise<T> {
+    const response = await axios.get<T>(url);
     return response.data;
   }
 
-  public async post(url: string, data: any): Promise<any> {
-    const response = await axios.post(url, data);
+  public async post<T>(url: string, data: any): Promise<T> {
+    const response = await axios.post<T>(url, data);
     return response.data;
   }
 
-  public async put(url: string, data: any): Promise<any> {
-    const response = await axios.put(url, data);
+  public async put<T>(url: string, data: any): Promise<T> {
+    const response = await axios.put<T>(url, data);
     return response.data;
   }
 
-  public async delete(url: string): Promise<any> {
-    const response = await axios.delete(url);
+  public async patch<T>(url: string, data: any): Promise<T> {
+    const response = await axios.patch<T>(url, data);
+    return response.data;
+  }
+
+  public async delete<T>(url: string): Promise<T> {
+    const response = await axios.delete<T>(url);
     return response.data;
   }
 }
